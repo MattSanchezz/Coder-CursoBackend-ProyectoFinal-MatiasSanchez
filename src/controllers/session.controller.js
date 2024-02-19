cd813c8f75341833e33b32d1c539e0d41009336a
MattSanchezz <mati.sanchez.2001@hotmail.com>
2024-02-19 02:45:51 -0300

    arreglos en routes y controllers

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,16 +1,74 @@
-export const getCurrentUser = (req, res) => {
-    if (req.isAuthenticated()) {
-      res.status(200).json({
-        status: "success",
-        data: {
-          user: req.user,
-        },
-      });
-    } else {
-      res.status(401).json({
-        status: "error",
-        message: "Usuario no autenticado",
-        data: {},
-      });
+import passport from 'passport';
+import { usersManager } from '../dao/usersManager.js';
+import { log } from '../logger.js';
+
+async function loginUser(req, res) {
+  passport.authenticate('login', {
+    failureRedirect: '/login',
+    failureFlash: true,
+  })(req, res, async () => {
+    try {
+      const user = await usersManager.findById(req.user._id);
+      user.last_connection = new Date();
+      await user.save();
+      res.redirect('/dashboard');
+    } catch (error) {
+      console.error('Error al obtener la información del usuario:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
     }
-  };
+  });
+}
+
+async function signupUser(req, res) {
+  passport.authenticate('signup', {
+    failureRedirect: '/signup',
+    failureFlash: true,
+  })(req, res, async () => {
+    try {
+      const user = await usersManager.findById(req.user._id);
+      user.last_connection = new Date();
+      await user.save();
+      res.redirect('/dashboard');
+    } catch (error) {
+      console.error('Error al obtener la información del usuario:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+}
+
+function renderDashboard(req, res) {
+  res.render('dashboard', { user: req.user });
+}
+
+function getCurrentUser(req, res) {
+  if (req.isAuthenticated()) {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: req.user,
+      },
+    });
+  } else {
+    res.status(401).json({
+      status: 'error',
+      message: 'Usuario no autenticado',
+      data: {},
+    });
+  }
+}
+
+export const handleLogin = async (req, res) => {
+  try {
+    const user = await usersManager.findById(req.user._id);
+
+      user.last_connection = new Date();
+      await user.save();
+
+      res.redirect('/dashboard');
+    } catch (error) {
+      log('ERROR', error.message);
+      res.status(500).json({ error: 'Error al obtener la información del usuario' });
+    }
+  }
+
+export { loginUser, signupUser, renderDashboard, getCurrentUser };
--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,57 +1,16 @@
 import express from 'express';
-import passport from 'passport';
-import { getCurrentUser } from '../controllers/session.controller.js';
+import { renderDashboard, signupUser, loginUser, getCurrentUser, handleLogin } from '../controllers/session.controller.js';
 import { isAuthenticated, isNotAuthenticated } from '../middleware/authenticationMiddle.js';
-import { log } from '../logger.js';
-import { usersManager } from '../dao/usersManager.js';
 
 const sessionRouter = express.Router();
 
 sessionRouter.get('/current', isAuthenticated, getCurrentUser);
 
-sessionRouter.post(
-  '/login',
-  isNotAuthenticated,
-  passport.authenticate('login', {
-    failureRedirect: '/login',
-    failureFlash: true,
-  }),
-  async (req, res) => {
-    try {
-      const user = await usersManager.findById(req.user._id);
+sessionRouter.post('/login', isNotAuthenticated, handleLogin, loginUser);
 
-      user.last_connection = new Date();
-      await user.save();
+sessionRouter.post('/signup', isNotAuthenticated, signupUser);
 
-      res.redirect('/dashboard');
-    } catch (error) {
-      log('ERROR', error.message);
-      res.status(500).json({ error: 'Error al obtener la información del usuario' });
-    }
-  }
-);
-sessionRouter.post(
-  '/signup',
-  passport.authenticate('signup', {
-    failureRedirect: '/signup',
-    failureFlash: true,
-  }),
-  async (req, res) => {
-    try {
-      const user = await usersManager.findById(req.user._id);
-      user.last_connection = new Date();
-      await user.save();
-      res.redirect('/dashboard');
-    } catch (error) {
-      console.error('Error al obtener la información del usuario:', error);
-      res.status(500).json({ error: 'Error interno del servidor' });
-    }
-  }
-);
-
-sessionRouter.get('/dashboard', isAuthenticated, (req, res) => {
-  res.render('dashboard', { user: req.user });
-});
+sessionRouter.get('/dashboard', isAuthenticated, renderDashboard);
 
 sessionRouter.get('/login', isNotAuthenticated, (req, res) => {
 });
