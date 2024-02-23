62ba2d8bdddaf5b401a7fe326ecc13964eebb3a0
MattSanchezz <mati.sanchez.2001@hotmail.com>
2024-02-23 00:51:34 -0300

    modificaciones

--- src/db/configDB.js
+++ src/db/configDB.js
@@ -5,5 +5,5 @@ import config from "../../config.js";
 const URI = config.mongo_uri;
 
 mongoose.connect(URI)
-.then((db) => log('INFO', 'Base de Datos conectada'))
-  .catch((error) => log('ERROR', `Error al conectar la base de datos: ${error.message}`));
+.then((db) => console.log('INFO', 'Base de Datos conectada'))
+  .catch((error) => console.log('ERROR', `Error al conectar la base de datos: ${error.message}`));
